75dd8a89281d056712bfc30b5d28cdf06b849c8e
polodev <polodev10@gmail.com>
2017-03-30 13:08:55 +0600

    added web loader paractice file

---
+++ src/components/Message.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="box">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style>
+  .box {
+    padding: 10px;
+    background: #e6e6e6;
+    border: 1px solid #d3d3d3;
+    margin-bottom: 10px;
+  }
+</style>
